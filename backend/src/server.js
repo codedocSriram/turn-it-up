ddc1ac043b316536c2507d4009d64d88ef96d90a
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-07 02:38:53 +0530

    Added MongoDB connection under lib folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { connectDB } from "./lib/db.js";
+
 import userRouter from "./routes/user.router.js";
 import authRouter from "./routes/auth.router.js";
 import adminRouter from "./routes/admin.router.js";
 import songRouter from "./routes/song.router.js";
 import albumRouter from "./routes/album.router.js";
 import statRouter from "./routes/stat.router.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
@@ -19,5 +22,6 @@ app.use("/api/albums", albumRouter);
 app.use("/api/stats", statRouter);
 
 app.listen(PORT, () => {
+    connectDB();
     console.log("Server is running in PORT:", PORT);
 });

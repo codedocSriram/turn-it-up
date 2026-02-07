02b45658600437c1c817517a20b9f9467ba9dce9
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-07 10:21:20 +0530

    Added madeForYou, trendingSongs methods in song controller

--- backend/src/controller/song.controller.js
+++ backend/src/controller/song.controller.js
@@ -32,3 +32,49 @@ export const getFeaturedSongs = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getMadeForYouSongs = async (req, res, next) => {
+    try {
+        const songs = await Song.aggregate([
+            {
+                $sample: { size: 4 },
+            },
+            {
+                $project: {
+                    _id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                },
+            },
+        ]); //getting 4 random songs from DB to show in MadeForYou component
+        res.status(200).json(songs);
+    } catch (error) {
+        console.log("Error in getMadeForYouSongs:", error.message);
+        next(error);
+    }
+};
+
+export const getTrendingSongs = async (req, res, next) => {
+    try {
+        const songs = await Song.aggregate([
+            {
+                $sample: { size: 4 },
+            },
+            {
+                $project: {
+                    _id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                },
+            },
+        ]); //getting 4 random songs from DB to show in Trending component
+        res.status(200).json(songs);
+    } catch (error) {
+        console.log("Error in getTrendingSongs:", error.message);
+        next(error);
+    }
+};
--- backend/src/routes/song.router.js
+++ backend/src/routes/song.router.js
@@ -2,11 +2,14 @@ import express from "express";
 import {
     getAllSongs,
     getFeaturedSongs,
+    getMadeForYouSongs,
+    getTrendingSongs,
 } from "../controller/song.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 const songRouter = express.Router();
 
 songRouter.get("/", protectRoute, requireAdmin, getAllSongs);
 songRouter.get("/featured", getFeaturedSongs);
-
+songRouter.get("/made-for-you", getMadeForYouSongs);
+songRouter.get("/trending", getTrendingSongs);
 export default songRouter;

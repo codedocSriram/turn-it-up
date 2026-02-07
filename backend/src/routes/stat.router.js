6a731ffd384ba76d41b7d295e83c36603c882546
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-07 10:37:24 +0530

    Added getStats method under stat controller

--- backend/src/routes/stat.router.js
+++ backend/src/routes/stat.router.js
@@ -1,9 +1,9 @@
 import express from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { getStats } from "../controller/stat.controller.js";
 
 const statRouter = express.Router();
 
-statRouter.get("/", (req, res) => {
-    res.send("stats Page");
-});
+statRouter.get("/", protectRoute, requireAdmin, getStats);
 
 export default statRouter;

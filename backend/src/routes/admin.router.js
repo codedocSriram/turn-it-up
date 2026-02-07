c784e577cbcc4fd21a87df53a14d113008b22e15
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-07 08:00:48 +0530

    Added deleteAlbum method under admin controller

--- backend/src/routes/admin.router.js
+++ backend/src/routes/admin.router.js
@@ -3,6 +3,7 @@ import {
     createSong,
     deleteSong,
     createAlbum,
+    deleteAlbum,
 } from "../controller/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 const adminRouter = express.Router();
@@ -10,4 +11,5 @@ const adminRouter = express.Router();
 adminRouter.post("/songs", protectRoute, requireAdmin, createSong);
 adminRouter.post("/albums", protectRoute, requireAdmin, createAlbum);
 adminRouter.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
+adminRouter.delete("/albums/:id", protectRoute, requireAdmin, deleteAlbum);
 export default adminRouter;

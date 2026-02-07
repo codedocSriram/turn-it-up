bf9367b6e0c39d3e67e5945a3a428f50d926cbde
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-07 07:55:31 +0530

    Added createAlbum methon under admin controller

--- backend/src/routes/admin.router.js
+++ backend/src/routes/admin.router.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { createSong, deleteSong } from "../controller/admin.controller.js";
+import {
+    createSong,
+    deleteSong,
+    createAlbum,
+} from "../controller/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 const adminRouter = express.Router();
 
 adminRouter.post("/songs", protectRoute, requireAdmin, createSong);
+adminRouter.post("/albums", protectRoute, requireAdmin, createAlbum);
 adminRouter.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
 export default adminRouter;

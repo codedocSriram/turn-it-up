89c010f280b731f5923542aa69fc0a0502b81f42
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-09 03:52:27 +0530

    Solved version mismatch of Shadcn Resizable component and added Sidebars

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
 function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<HomePage />} />
                 <Route
                     path="/sso-callback"
                     element={
@@ -16,6 +16,9 @@ function App() {
                     }
                 />
                 <Route path="/auth-callback" element={<AuthCallbackPage />} />
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<HomePage />} />
+                </Route>
             </Routes>
         </>
     );

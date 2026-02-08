bf5d018c540c34009947b9b10b2366732d633335
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-09 04:36:19 +0530

    Worked on LeftSidebar component

--- frontend/src/components/SignInOAuthButtons.tsx
+++ frontend/src/components/SignInOAuthButtons.tsx
@@ -1,6 +1,6 @@
 import { useSignIn } from "@clerk/clerk-react";
 
-import { Button } from "./button";
+import { Button } from "./ui/button";
 
 const SignInOAuthButtons = () => {
     const { signIn, isLoaded } = useSignIn();

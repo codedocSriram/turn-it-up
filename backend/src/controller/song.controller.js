b1cd4ce38298feb31f5804755c6ca246f3cd3a9f
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-09 11:03:10 +0530

    modified res.json(songs) in back end

--- backend/src/controller/song.controller.js
+++ backend/src/controller/song.controller.js
@@ -3,7 +3,7 @@ import { Song } from "../models/song.model.js";
 export const getAllSongs = async (req, res, next) => {
     try {
         const songs = await Song.find().sort({ createdAt: -1 });
-        res.status(200).json({ songs });
+        res.status(200).json(songs);
     } catch (error) {
         console.log("Error in getAllSongs:", error.message);
         next(error);

ef09d2d01d91dc30536ed7637666b5af888cf8e3
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-09 11:08:38 +0530

    Used musicStore in leftSidebar component to display temp Albums

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -2,12 +2,19 @@ import PlaylistSkeleton from "@/components/skeletons/PlaylistSkeleton";
 import { buttonVariants } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
+import { useMusicStore } from "@/store/useMusicStore";
 import { SignedIn } from "@clerk/clerk-react";
 import { HomeIcon, Library, MessageCircle } from "lucide-react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const LeftSidebar = () => {
-    const isLoading = true;
+    const { albums, fetchAlbums, isLoading } = useMusicStore();
+
+    useEffect(() => {
+        fetchAlbums();
+    }, [fetchAlbums]);
+    console.log(albums);
     return (
         <div className="h-full flex flex-col gap-2">
             {/* Nav menu */}
@@ -55,7 +62,31 @@ const LeftSidebar = () => {
 
                 <ScrollArea className="h-[calc(100vh-300px)]">
                     <div className="space-y-2">
-                        {isLoading ? <PlaylistSkeleton /> : "Musics"}
+                        {isLoading ? (
+                            <PlaylistSkeleton />
+                        ) : (
+                            albums.map((album) => (
+                                <Link
+                                    to={`/albums/${album._id}`}
+                                    key={album._id}
+                                    className="p-2 hover:bg-zinc-800 rounded-md flex items-center gap-3 group cursor-pointer"
+                                >
+                                    <img
+                                        src={album.imageUrl}
+                                        alt="Playlist img"
+                                        className="size-12 rounded-md flex-shrink-0 object-cover"
+                                    />
+                                    <div className="flex-1 min-w-0 hidden md:block">
+                                        <p className="font-medium truncate">
+                                            {album.title}
+                                        </p>
+                                        <p className="text-sm text-zinc-400 truncate">
+                                            Album • {album.artist}
+                                        </p>
+                                    </div>
+                                </Link>
+                            ))
+                        )}
                     </div>
                 </ScrollArea>
             </div>

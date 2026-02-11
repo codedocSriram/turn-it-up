ca5c15b1965b616d1ced60ddea54a2cb1416b76c
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-11 11:32:23 +0530

    Fixed some UI

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,30 +1,45 @@
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
+import {
+    SignedIn,
+    SignedOut,
+    SignOutButton,
+    UserButton,
+} from "@clerk/clerk-react";
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import SignInOAuthButtons from "./SignInOAuthButtons";
+import { useAuthStore } from "@/store/useAuthStore";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 const Topbar = () => {
-    const isAdmin = false;
+    const { isAdmin } = useAuthStore();
+    console.log({ isAdmin });
     return (
         <div
             className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75
     backdrop-blur-md z-10
     "
         >
-            <div className="flex gap-2 items-center">TurnItUp</div>
+            <div className="flex gap-2 items-center">
+                <img src="/turnituplogo.svg" className="size-8" alt="logo" />
+                TurnItUp
+            </div>
             <div className="flex items-center gap-4">
                 {isAdmin && (
-                    <Link to={"/admin"}>
+                    <Link
+                        to={"/admin"}
+                        className={cn(buttonVariants({ variant: "outline" }))}
+                    >
                         <LayoutDashboardIcon className="size-4 mr-2" />
                         Admin Dashboard
                     </Link>
                 )}
-                <SignedIn>
-                    <SignOutButton />
-                </SignedIn>
+
                 <SignedOut>
                     <SignInOAuthButtons />
                 </SignedOut>
+
+                <UserButton />
             </div>
         </div>
     );
